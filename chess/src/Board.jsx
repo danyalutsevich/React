55a0fae8fc57215ad489ec82674af13c535b7f49
danyalutsevich <luchevich31@gmail.com>
2022-07-13 02:26:20 +0300

    available moves

--- chess/src/Board.jsx
+++ chess/src/Board.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './Chess.css'
 import { Chess } from "chess.js";
 
 
@@ -13,11 +12,9 @@ export default class Board extends React.Component {
             board: [],
             pieces: [],
             files: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-            a: 10,
-            b: 20,
             selectedPiece: '',
             from: '',
-            to: ''
+            availableMoves: [],
         }
     }
 
@@ -27,9 +24,6 @@ export default class Board extends React.Component {
             files,
             chess,
         } = this.state;
-
-        // console.log(chess.move('Nf3'))
-        // console.log('e4', chess.board())
 
         let tempBoard = []
         let row = []
@@ -48,6 +42,40 @@ export default class Board extends React.Component {
 
     }
 
+    getPiece(piece) {
+        return piece != null ?
+            < img src={`./Icons/${piece?.color + piece?.type}.svg`} alt={`${piece?.type}`} className={piece?.color + "Piece"} />
+            : null
+    }
+
+    getAvailableMoves(from) {
+
+        const {
+            files,
+            chess,
+        } = this.state
+
+        let availableMovesTemp = []
+
+        let currentMove
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+
+                currentMove = chess.move({ from: from, to: files[i].concat(j + 1) })
+
+                if (currentMove != null) {
+
+                    availableMovesTemp.push({ f: i, r: 7 - j })
+                    chess.undo()
+
+                }
+            }
+        }
+
+        this.setState({ availableMoves: availableMovesTemp })
+
+    }
 
     componentDidMount() {
 
@@ -60,43 +88,40 @@ export default class Board extends React.Component {
 
     }
 
-    getPiece(piece) {
-
-        return piece != null ?
-            < img src={`./Icons/${piece?.type}.svg`} alt={`${piece?.type}`} className={piece?.color + "Piece"} />
-            : null
-    }
 
     onDragStartHandler(e, cell, rindex, cindex) {
 
         e.target.style.cursor = 'grabbing'
-        // console.log("drag", cell, rindex, cindex)
 
+
+        console.log("drag", cell, rindex, cindex)
         let piece = this.state.chess.board()[cindex][rindex]
 
         this.setState({ selectedPiece: piece.type })
         this.setState({ from: piece.square })
-        this.setState({ to: '' })
+
+        this.getAvailableMoves(piece.square)
 
     }
 
     onDragEnterHandler(e) {
-
+        // console.log('enter')
         // e.target.style = "filter:invert(50%)"
     }
 
     onDragLeaveHandler(e) {
-
+        // console.log('leave')
         // e.target.style = "filter:invert(-50%)"
     }
 
     onDragEndHandler(e) {
 
         // e.target.style = "filter:invert(-50%)"
+        // console.log('end')
     }
 
     onDragOverHandler(e) {
-
+        // console.log('over')
         e.preventDefault()
     }
 
@@ -105,21 +130,24 @@ export default class Board extends React.Component {
         e.preventDefault()
 
         const {
-            selectedPiece,
+            from,
+            chess
         } = this.state
 
 
 
-        let to = selectedPiece.toUpperCase().concat(cell.file.concat(cell.rank)).replace('P','')
+        let to = cell.file.concat(cell.rank)
 
 
-        console.log(to)
+        console.log('drop', to)
 
-        this.state.chess.move(to)
+        console.log('move', this.state.chess.move({ from: from, to: to }))
 
         this.setState({ pieces: this.state.chess.board() })
 
         // console.log("drop", cell)
+
+
     }
 
 
@@ -128,7 +156,8 @@ export default class Board extends React.Component {
         const {
             board,
             files,
-            pieces
+            pieces,
+            chess,
         } = this.state;
 
 
@@ -145,9 +174,12 @@ export default class Board extends React.Component {
         return (
 
             <div className="Board">
+                {chess.turn() === 'w' ?
+                    <div className="whiteCell Turn">turn</div> :
+                    <div className="blackCell Turn">turn</div>
 
-                <div className="a" draggable={true} onDragStartCapture={() => { this.setState({ a: "captured" }) }}>{this.state.a}</div>
-                <div className="b" draggable={true}>{this.state.b}</div>
+
+                }
                 <div className="Coordinates">
                     <div className="Ranks">
                         {[...Array(8).keys()].reverse().map((rankNumber) =>
@@ -163,36 +195,34 @@ export default class Board extends React.Component {
                     </div>
                 </div>
 
-                {board.map((row, rindex) =>
+                {board.map((row, fileindex) =>
                     <div key={row[0].file}>
-                        {row.map((cell, cindex) => {
-                            return cell.color === 0 ?
+                        {row.map((cell, rankindex) => {
+                            return (
 
-                                <div className="whiteCell" key={cell.file + cell.rank}
+                                <div className=
+                                    {
 
-                                    onDragStart={(e) => this.onDragStartHandler(e, cell, rindex, cindex)}
+                                        (cell.color === 0 ? "whiteCell" : "blackCell")
+                                            .concat(this.state.availableMoves.some(
+                                                (element) => element.f == fileindex && element.r == rankindex) ? " availableMove" : "")
+
+                                    }
+
+                                    key={cell.file + cell.rank}
+
+                                    onDragStart={(e) => this.onDragStartHandler(e, cell, fileindex, rankindex)}
                                     onDragEnter={(e) => this.onDragEnterHandler(e)}
                                     onDragLeave={(e) => this.onDragLeaveHandler(e)}
                                     onDragEnd={(e) => this.onDragEndHandler(e)}
                                     onDragOver={(e) => this.onDragOverHandler(e)}
                                     onDrop={(e) => this.onDropHandler(e, cell)}
                                 >
-                                    {this.getPiece(pieces[cindex][rindex])}
+                                    {this.getPiece(pieces[rankindex][fileindex])}
+
                                 </div>
 
-                                :
-
-                                <div className="blackCell" key={cell.file + cell.rank}
-
-                                    onDragStart={(e) => this.onDragStartHandler(e, cell, rindex, cindex)}
-                                    onDragEnter={(e) => this.onDragEnterHandler(e)}
-                                    onDragLeave={(e) => this.onDragLeaveHandler(e)}
-                                    onDragEnd={(e) => this.onDragEndHandler(e)}
-                                    onDragOver={(e) => this.onDragOverHandler(e)}
-                                    onDrop={(e) => this.onDropHandler(e, cell)}
-                                >
-                                    {this.getPiece(pieces[cindex][rindex])}
-                                </div>
+                            )
                         })}
                     </div>
                 )}
---
+++ chess/src/EndGameModal.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export default class EndGameModal extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+
+
+    }
+
+    render(){
+
+        return(
+            <div>
+                
+            </div>
+        )
+
+    }
+
+
+}
+
